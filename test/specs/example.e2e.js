ccb1de6b70d4d55b2282278b89f51b17710250e4
DmytroMaslo <maslo.dmitry@gmail.com>
2023-01-06 17:30:00 +0200

    _$ function

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,5 +1,26 @@
 import LoginPage from '../pageobjects/login.page.js'
 import SecurePage from '../pageobjects/secure.page.js'
+import {Element,ElementArray} from 'webdriverio'
+
+const lazy = ['$','$$']
+
+function _$(selector, parentHandler){
+    async function getCurrentElement(){
+        if(parentHandler){
+            const parent = await parentHandler()
+            return parent.$(selector)
+        } else {
+            return $(selector);
+        }
+    }
+    return new Proxy({},{
+        get(_t,value){
+            if(lazy.includes(value)){
+                return(selector)=>_$(_selector,getCurrentElement);
+            }
+        }
+    })
+}
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
